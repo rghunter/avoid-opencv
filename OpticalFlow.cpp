e9cfa4c74dd14729184b662f1a22c804c4eb8302
Ryan G Hunter <rghunter@bu.edu>
2012-09-05 21:16:32 -0400

    testing program

--- OpticalFlow.cpp
+++ OpticalFlow.cpp
@@ -7,6 +7,8 @@
 #include <stdlib.h>
 #include <fstream>
 
+#define ACC_ERROR 2
+
 using namespace cv;
 using namespace std;
 
@@ -15,33 +17,55 @@ Mat frame_corrected;
 Mat fixed_descriptor;
 vector<KeyPoint> keypoint_fixed;
 
+float distance(KeyPoint *pt1, KeyPoint *pt2)
+{
+	//float x = pow(pt1->x,2);
+	//float y = pt1->y;
+	//return x+y;
+	return sqrt(pow(((float)pt1->pt.x - pt2->pt.x),2)+pow((pt1->pt.y - pt2->pt.y),2));
+}
+
+
 int main(void)
 {
+	//VideoCapture video("./static.mp4");
+	VideoCapture video(0);
 
-	SurfFeatureDetector detector(5000,4);
-	VideoCapture video("./croppedTest2.mp4");
-	//video.set(CV_CAP_PROP_FRAME_WIDTH,640);
-	//video.set(CV_CAP_PROP_FRAME_HEIGHT,480);
-	//GridAdaptedFeatureDetector detector(new SurfFeatureDetector(2000,4), 100, 4,4);
-	FREAK extractor(true,true,30,2);
-	BruteForceMatcher <HammingLUT> matcher;
+
+	SurfFeatureDetector detector(4000,1);
+	FREAK extractor(true,true,70,1);
+	BruteForceMatcher <Hamming> matcher;
 	vector<KeyPoint> keypoint;
 	Mat descriptor;
 	Mat raw_frame;
 	Mat input;
 	Mat output_frame;
-	vector<vector<DMatch> > matches;
-	//vector<DMatch> matches;
+	//vector<vector<DMatch> > matches;
+	vector<DMatch> matches;
 	bool first = true;
-	ofstream data_log;
-	data_log.open("./distance.csv");
+	ofstream data_log0, data_log1;
+	data_log0.setf(ios::floatfield);
+	data_log0.precision(5);
+	data_log0.open("./hitMissEval.csv");
 
-	while(video.grab())
+	vector<vector<char> > MASK;
+
+	//MASK.zeros(100,100,CV_8U);
+	//MASK.col(1) = 1;
+
+	data_log0 << "Hit, Miss, Total" << endl;
+
+	//double frame_count = video.get(CV_CAP_PROP_FRAME_COUNT);
+	int current_count = 0;
+
+	while((current_count < 100)) //|| (frame_count - video.get(CV_CAP_PROP_POS_FRAMES)) > 1)
 	{
 		video >> input;
-		if(!input.empty())
+		current_count += 1;
+		if(true)
 		{
-			cvtColor(input,raw_frame,CV_RGB2GRAY);
+			//raw_frame = input.clone();
+			cvtColor(input,raw_frame,CV_BGR2GRAY);
 			if(first)
 			{
 				fixed_image = raw_frame.clone();
@@ -53,28 +77,54 @@ int main(void)
 
 			detector.detect(raw_frame,keypoint);
 			extractor.compute(raw_frame,keypoint,descriptor);
-			//matcher.match(descriptor,fixed_descriptor,matches);
-			matcher.radiusMatch(descriptor, fixed_descriptor,matches,100,Mat(),true);
+			matcher.match(descriptor,fixed_descriptor,matches);
+		//	matcher.radiusMatch(descriptor, fixed_descriptor,matches,1,Mat(),true);
 			float sum = 0;
-			for(int i=0;i<matches.size();i++)
+			float len = 0;
+			vector<char> MASK;
+			float correct, incorrect;
+			correct = 0;
+			incorrect = 0;
+			for(uint i=0;i<matches.size();i++)
 			{
-				data_log << matches[i][1].distance << ", ";
+
+				KeyPoint pt1 = keypoint_fixed[matches[i].trainIdx];
+				KeyPoint pt2 = keypoint[matches[i].queryIdx];
+
+				float euclidian_distance = distance(&keypoint_fixed[matches[i].trainIdx],&keypoint[matches[i].queryIdx]);
+				cout << euclidian_distance << "\t";
+
+				if(euclidian_distance < ACC_ERROR)
+					correct += 1;
+				else
+					incorrect += 1;
+#if 0
+				if(matches[i].distance > 5)
+					MASK.push_back(0);
+				else
+				{
+					len += 1;
+					sum += distance(&keypoint_fixed[matches[i].trainIdx],&keypoint[matches[i].queryIdx]);
+					//cout << " " << distance(&keypoint_fixed[matches[i].trainIdx],&keypoint[matches[i].queryIdx]) << " ";
+					MASK.push_back(1);
+				}
+#endif
 			}
-			data_log << endl;
 
-			cout << "Average Distance: " << sum/(float)matches.size() << endl;
-
+			data_log0 << correct << ", " << incorrect << ", " << matches.size() <<  ", " << keypoint_fixed.size()<< endl;
 
 
-			drawMatches(fixed_image,keypoint_fixed,raw_frame,keypoint,matches,output_frame);
-			imshow("output",output_frame);
-			std::cout << "Num Points: " << keypoint.size() << "Num Matches: " << matches.size() << std::endl;
-			//drawKeypoints(raw_frame,keypoint,raw_frame,Scalar(255,0,0));
-			//drawKeypoints(raw_frame,keypoint_fixed,raw_frame,Scalar(0,255,0),DrawMatchesFlags::DRAW_OVER_OUTIMG);
-			imshow("FLOW!", raw_frame);
+		//	cout << "Average Distance: " << (float)(sum/len) << endl;
+			cout << endl;
+
+			//drawMatches(fixed_image,keypoint_fixed,raw_frame,keypoint,matches,output_frame,Scalar::all(-1),Scalar::all(-1),MASK);
+			//imshow("output",output_frame);
+			//cout << "Num Points: " << keypoint.size() << " Num Matches: " << matches.size() << endl;
 		}
-		waitKey(1);
+		//waitKey(1);
 	}
-	data_log.close();
+	cout << "Done!" << endl;
+	data_log0.close();
+	video.release();
 	return 0;
 }
